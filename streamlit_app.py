a9c0f0f469ba2ec3784d95fc3f911ae69b7f1504
jzotter7205 <143535723+jzotter7205@users.noreply.github.com>
2023-11-15 17:42:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -38,7 +38,7 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
-streamlit.header("The fruit load list contains:")
+streamlit.header('The fruit load list contains:')
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
